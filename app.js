e945c12d2698dc3d37c695943f78a6b1d5e78ee3
Bobbie <34404266+legoandmars@users.noreply.github.com>
2021-10-07 16:00:39 -0700

    Add support for non-gh mods (temp)

--- app.js
+++ app.js
@@ -8,34 +8,50 @@ let inputJSON = fs.readFileSync('Data/mod_data.json');
 let modList = JSON.parse(inputJSON);
 let result = []
 
-modList.mods.forEach(mod => 
-  https.get(`${prefix}${mod.gitPath}${postfix}`, { headers: { 'User-Agent' : 'DeadlyKitten/NASB-Mod-Info' ,'Authorization': `Token ${process.env.SECRET}`}},(res) => {
-    let body = "";
-
-      res.on("data", (chunk) => {
-          body += chunk;
-      });
-
-      res.on("end", () => {
-          try {
-            let json = JSON.parse(body);
-            result.push({
-              'name': mod.name,
-              'author': mod.author,
-              'description': mod.description,
-              'version': json.tag_name.replace(/[^\d\n,.]/g,''),
-              'dependencies': mod.dependencies ?? [],
-              'dependents': mod.dependents,
-              'install_location': mod.installPath,
-              'git_path': mod.gitPath,
-              'group': mod.group,
-              'download_url': json.assets[mod.releaseId].browser_download_url
-            });
-          } catch (error) {
-              console.error(error.message);
-          };
+modList.mods.forEach(mod => {
+  if(mod.downloadLink != null && mod.version != null){
+    result.push({
+      'name': mod.name,
+      'author': mod.author,
+      'description': mod.description,
+      'version': mod.version,
+      'dependencies': mod.dependencies ?? [],
+      'dependents': mod.dependents,
+      'install_location': mod.installPath,
+      'git_path': mod.gitPath,
+      'group': mod.group,
+      'download_url': mod.downloadLink
     });
-}));
+  }else{
+    https.get(`${prefix}${mod.gitPath}${postfix}`, { headers: { 'User-Agent' : 'DeadlyKitten/NASB-Mod-Info' ,'Authorization': `Token ${process.env.SECRET}`}},(res) => {
+      let body = "";
+
+        res.on("data", (chunk) => {
+            body += chunk;
+        });
+
+        res.on("end", () => {
+            try {
+              let json = JSON.parse(body);
+              result.push({
+                'name': mod.name,
+                'author': mod.author,
+                'description': mod.description,
+                'version': json.tag_name.replace(/[^\d\n,.]/g,''),
+                'dependencies': mod.dependencies ?? [],
+                'dependents': mod.dependents,
+                'install_location': mod.installPath,
+                'git_path': mod.gitPath,
+                'group': mod.group,
+                'download_url': json.assets[mod.releaseId].browser_download_url
+              });
+            } catch (error) {
+                console.error(error.message);
+            };
+      });
+    });
+  }
+});
 
 let attempts = 0;
 let timeout = 20;
